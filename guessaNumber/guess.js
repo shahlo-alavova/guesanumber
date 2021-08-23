b7ef490651fa79ddd82379ddf69f681170385614
shahlo-alavova <64728278+shahlo-alavova@users.noreply.github.com>
2021-08-23 12:11:47 +0600

    Add files via upload

---
+++ guessaNumber/guess.js
@@ -0,0 +1,6 @@
+
+var secretNumber = 7
+var number = Number(prompt("Guess a number!"))
+if(number === secretNumber){alert("Correct!")}
+else if(number > secretNumber) {alert("Too high")}
+else {alert("Too low!")}
